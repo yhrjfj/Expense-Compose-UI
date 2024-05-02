747903c5b2d1636bd4f90b2c816e778204a5fd7d
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-05-02 15:23:54 +0600

    What need to create

--- app/src/main/java/com/yhr/jfj/expense/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/expense/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.yhr.jfj.expense.ui.theme.ExpenseTheme
 
@@ -20,7 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExpenseTheme {
                 SetBarColor(color = MaterialTheme.colorScheme.background)
-
+                HomeScreen()
             }
         }
     }
@@ -36,6 +43,29 @@ class MainActivity : ComponentActivity() {
             systemUiController.setSystemBarsColor(
                 color = color
             )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            TODO("Create bottom bar")
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            // Create wallet section
+            // Create card section
+            Spacer(modifier = Modifier.height(16.dp))
+            // Create finance section
+            // Create currency section
         }
     }
 }
