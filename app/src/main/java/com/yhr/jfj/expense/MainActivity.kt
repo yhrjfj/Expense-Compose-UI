3ccdd76f29f6cd52408fb09b962553148f52c3fb
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-05-02 15:16:46 +0600

    Change status bar color

--- app/src/main/java/com/yhr/jfj/expense/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/expense/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.yhr.jfj.expense.ui.theme.ExpenseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExpenseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SetBarColor(color = MaterialTheme.colorScheme.background)
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpenseTheme {
-        Greeting("Android")
+    // Set status bar color
+    @Composable
+    fun SetBarColor(
+        modifier: Modifier = Modifier,
+        color: Color
+    ) {
+        val systemUiController = rememberSystemUiController()
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = color
+            )
+        }
     }
 }
