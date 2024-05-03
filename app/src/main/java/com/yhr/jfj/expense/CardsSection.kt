1b39b1c09aa42a97c45764ad254f5bea5a363a3f
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-05-03 10:09:49 +0600

    Make card clickable

--- app/src/main/java/com/yhr/jfj/expense/CardsSection.kt
+++ app/src/main/java/com/yhr/jfj/expense/CardsSection.kt
@@ -2,6 +2,7 @@ package com.yhr.jfj.expense
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -103,6 +104,7 @@ fun CardItem(modifier: Modifier = Modifier, index: Int) {
                 .background(card.cardColor)
                 .width(250.dp)
                 .height(170.dp)
+                .clickable { }
                 .padding(vertical = 12.dp, horizontal = 16.dp),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
